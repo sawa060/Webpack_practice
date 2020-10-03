911de81bd46b8be05ef6cdf261adefe2bf2354f9
澤田優作 <>
2020-10-03 23:41:06 +0900

    Generate HTML through Pug

--- webpack.config.js
+++ webpack.config.js
@@ -4,45 +4,64 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  entry: './src/javascripts/main.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'javascripts/main.js'  // 出力されるファイル名
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader
-          },
-          {
-            loader: 'css-loader'
-          }
-        ]
-      },
-      {
-        test: /\.(png|jpg)/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              esModule: false,
-              name: 'images/[name].[ext]'
-            }
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: './stylesheets/main.css'
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/templates/index.html'
-    }),
-    new CleanWebpackPlugin()
-  ]
-}
+	entry: "./src/javascripts/main.js",
+	output: {
+		path: path.resolve(__dirname, "./dist"),
+		filename: "javascripts/main.js" // 出力されるファイル名
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader
+					},
+					{
+						loader: "css-loader"
+					}
+				]
+			},
+			{
+				test: /\.(png|jpg)/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							esModule: false,
+							name: "images/[name].[ext]"
+						}
+					}
+				]
+			},
+			{
+				test: /\.pug/,
+				use: [
+					{
+						loader: "html-loader"
+					},
+					{
+						loader: "pug-html-loader",
+						options: {
+							pretty: true
+						}
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "./stylesheets/main.css"
+		}),
+		new HtmlWebpackPlugin({
+      template: "./src/templates/index.pug",
+      filename: 'index.html'
+		}),
+		new HtmlWebpackPlugin({
+      template: "./src/templates/access.pug",
+      filename: 'access.html'
+		}),
+		new CleanWebpackPlugin()
+	]
+};
