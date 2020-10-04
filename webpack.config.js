ea016a02159c477fcadbb557fe165ac6b3833f61
澤田優作 <>
2020-10-04 14:02:16 +0900

    use image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
 	entry: "./src/javascripts/main.js",
 	output: {
 		path: path.resolve(__dirname, "./dist"),
@@ -12,21 +14,38 @@ module.exports = {
 	module: {
 		rules: [
 			{
+				test: /\.js/,
+				exclude: "/node_modules/",
+				use: [
+					{
+						loader: "babel-loader",
+						options: {
+              presets: [
+                ["@babel/preset-env", { targets: "> 0.25%, not dead" }]
+              ]
+						}
+					}
+				]
+			},
+			{
 				test: /\.(css|sass|scss)/,
 				use: [
 					{
 						loader: MiniCssExtractPlugin.loader
 					},
 					{
-						loader: "css-loader"
-          },
-          {
-            loader: 'sass-loader'
-          }
+            loader: "css-loader",
+            options: {
+              sourceMap: false,
+            }
+					},
+					{
+            loader: "sass-loader",
+					}
 				]
 			},
 			{
-				test: /\.(png|jpg)/,
+				test: /\.(png|jpg|jpeg)/,
 				use: [
 					{
 						loader: "file-loader",
@@ -34,7 +53,16 @@ module.exports = {
 							esModule: false,
 							name: "images/[name].[ext]"
 						}
-					}
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              }
+            }
+          }
 				]
 			},
 			{
